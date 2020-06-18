ed1560892e6035e10315b93bf8687e3d8cac879b
jvlee6 <jvergaralee@gmail.com>
2020-06-18 18:59:19 -0400

    GUI

---
+++ src/main/java/com/me/curriculum_pdf/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.me.curriculum_pdf;
+
+import com.me.curriculum_pdf.gui.App;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author zdk
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame j = new App();
+        j.setVisible(true);
+    }
+    
+}
